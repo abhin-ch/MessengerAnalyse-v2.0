9fa90d081be3c952d7adc894adaf4b68349eb5c3
anth7310 <anthonyin98@hotmail.ca>
2020-09-17 17:34:03 -0400

    returned code redundicies

--- src/formator.py
+++ src/formator.py
@@ -5,17 +5,17 @@ DESCRIPTION: This script edits the file uploaded by the user. It will merge them
 """
 from glob import glob
 import json 
-import os
-import sys
+import os 
 import os.path # for file path
 
 
-# No Python Cache
-sys.dont_write_bytecode = True
+# merged json path
+MESSAGE_PATH = os.path.join('.', 'uploads', 'message.json')
 
-# Get the Path 
 
 def get_filename():
+    """ Return files in uploads folder
+    """
     f = os.path.join('.', 'uploads', 'message_*.json')
     filename = glob(f)
     return filename
@@ -25,11 +25,10 @@ def merge_message(filename):
         output and save the merged messages'''
     # Intializing
     data = []
-    num_file = 0
     
     # Interate through the files
     for file in filename:
-        num_file += 1
+
         # Open each file
         with open(file) as msg:
             # Get the messages from each file
@@ -40,31 +39,22 @@ def merge_message(filename):
     json_object = json.dumps(data, indent = 10) 
 
     # write json file object
-    msg = os.path.join('.', 'uploads', 'message.json')
-    with open(msg, "w") as outfile: 
+    with open(MESSAGE_PATH, "w") as outfile: 
         outfile.write(json_object)
 
-def merge_file(filename ):
-    ''' This will Merge the Json Files and 
-        output and save the merged file'''
-    # intializing
-    data = {}
+def merge_file(filename):
+    """ merge Message.json and message_1.json
+    """
     
     fil_url = os.path.join('.', 'uploads', 'message_1.json')
-    msg_url = os.path.join('.', 'uploads', 'message.json')
-    # Open file 
-    # -> Making a proper file
-    with open(msg_url) as msg:
-            message = json.load(msg)
-            data['messages'] = message
 
     with open(fil_url) as fil:
-            file = json.load(fil)
-            data['participants'] = file['participants']
-            data['title'] = file['title']
-            data['is_still_participant'] = file['is_still_participant']
-            data['thread_type'] = file['thread_type']
-            data['thread_path'] = file['thread_path']
+        data = json.load(fil)
+    
+    with open(MESSAGE_PATH) as msg:
+        message = json.load(msg)
+        data['messages'] = message
+
     # Write file 
     # json_object = json.dumps(data, indent = 4) 
     # with open(data_url, "w") as outfile: 
@@ -73,11 +63,9 @@ def merge_file(filename ):
     return data
 
 def delete_files(filename):
-    ''' Deleteing the files from the folder 
+    ''' Delete files from uploads folder
     '''
-    
-    message = os.path.join('.', 'uploads', 'message.json')
-    os.remove(message)
+    os.remove(MESSAGE_PATH)
     for file in filename:
         os.remove(file)
 
